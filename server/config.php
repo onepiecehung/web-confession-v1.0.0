3d1001999dd772c0600b87db6bb8cef48e503c6b
Nguyễn Hữu Hưng <360anime.suki@gmail.com>
2017-11-18 11:11:12 +0700

    Update config.php

--- server/config.php
+++ server/config.php
@@ -1,12 +1,13 @@
 <?php
 /* >_ Developed by Vy Nghia */
 require 'lib/class/confession.class.php';
-define('WEBURL', 'http://domain.com');
+define('WEBURL', 'https://webcf-onepiecehung.c9users.io');
 
 $db = new Database;
-$db->dbhost('localhost');
-$db->dbuser('username');
-$db->dbpass('password');
-$db->dbname('db_name');
+$db->dbhost('eu-cdbr-west-01.cleardb.com');
+$db->dbuser('bbcac040a59ad8');
+$db->dbpass('73b79c6d4f03864');
+$db->dbname('heroku_140e4f0e61c43cb');
 
 $db->connect();
+
